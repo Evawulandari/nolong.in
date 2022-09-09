fdd59cb7917ee62fde6db33407d6df8102928a0b
ridwan wiranata <ridwanwiranata0@gmail.com>
2022-09-09 11:24:10 +0700

    membuat blog

---
+++ assets/js/blog.js
@@ -0,0 +1,19 @@
+const blogs = [
+    {
+        judul: "Kenaikan BBM",
+        img: "BBM.jpg",
+        isi: "harga bahan bakar bukan hanya memengaruhi oleh pengemudi saja tetapi para penumpang pun ikut menglami kenaikan BBM.Pada setiap orang ketika ingin naik kendaraan umum ,maka yang merekqa keluhkan adalah kenaikan ongkos kendaraan. Ada sebagian orang yang ingin membeli bensin, hanya bisa membeli 1 liter bensin. Akhirnya si pengemudi itu harus mendorong motornya sampai tujuan. Inilah jadi keprihatinan pemerintah terhadap warga yang masih ragu untuk membeli bahan bakar.",
+       autor:"ridwan wiranata",
+       tanggal:"09-10-2022.",
+
+    },
+    {
+        judul: "Kenaikan Ayam",
+        img: "ayam1.jpeg",
+        isi: "Pada masa sekaranga, banyak sekali harga daging meningkat. Tak hanya harga daging, harga pakanpun sekarang cukup memperhatinkan. Menyebabkan orang setiap yang ingin makan daging di tunda sementara, tak hanya harga pakan relatif tinggi, ada sebagian besar daging ayam mahal karena populasinya pada setiap harinya, tak sedikitpun ayam yang masih hidup. ada sebagian mati karena sakit,cacata dan sebagainya.Yang menyebabkan daging mahal dan sedikitnya pemasokan",
+        autor:"ridwan wiranata",
+       tanggal:"09-10-2022.",
+
+    },
+    
+];
